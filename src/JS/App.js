3b59e0a6c95f897dbedda18c3e5beef869b83ded
Gold240sx <240designworks@gmail.com>
2022-08-30 13:58:04 -0500

    Basic Config ( 1st ommit)

---
+++ src/JS/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../CSS/app.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='underline text-3xl'>App</h1>
+    </div>
+  )
+}
+
+export default App
