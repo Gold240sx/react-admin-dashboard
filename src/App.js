322e804390587bd8fd9a3fe6292e381a8025c363
Gold240sx <240designworks@gmail.com>
2022-08-31 02:04:29 -0500

    ECommerce Section Completed

--- src/App.js
+++ src/App.js
@@ -39,9 +39,9 @@ const App = () => {
                 `dark:bg-main-bg bg-main-bg min-h-screen w-full
                 ${activeMenu ? 'md:ml-72' :'flex-2'}`
           }>
-            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-                <Navbar />
-            </div>
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+              <Navbar />
+          </div>
            
 
           {/* Main div for Routing */}
